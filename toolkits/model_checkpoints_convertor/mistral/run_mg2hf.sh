92167935701c7e28ed549d4ef89f53b56a13325e
root <zjuyfw@163.com>
2024-08-01 03:52:20 +0000

    convert use gpu memory

---
+++ toolkits/model_checkpoints_convertor/mistral/run_mg2hf.sh
@@ -0,0 +1,14 @@
+sh hf2mcore_convertor.sh \
+8x22B \
+/home/amd/fuweiy/cache/models/Mixtral-8x22B-v0.1 \
+../../../     \
+/home/amd/fuweiy/cache/models/megatron/Mixtral-8x22B-v0.1-to-mcore-tp8-pp2-ep2-exp8-ws32 \
+/home/amd/fuweiy/cache/models/mg2hf/Mixtral-8x22B-v0.1 \
+8  \
+2  \
+0  \
+8  \
+2  \
+2 \
+true \
+32
